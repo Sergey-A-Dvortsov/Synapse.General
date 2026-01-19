38d6bcabd695a29bf42c70666ab926a064dae858
Synapse <sergey.a.dvortsov@gmail.com>
2026-01-20 02:03:54 +0300

    struct Range modification

--- Range.cs
+++ Range.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Synapse.General
 {
-    public struct Range<T>
+    public struct Range<T> where T : INumber<T>
     {
         public Range() {}
 
@@ -15,7 +16,31 @@ namespace Synapse.General
             Max = max;
             Min = min;
         }
+
+        /// <summary>
+        /// Specifies a range using an absolute or relative offset from a given value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="offset"></param>
+        /// <param name="isper">if true, then relative offset</param>
+        public Range(T value, T offset, bool isper)
+        {
+            Max = isper ? value + (value * offset) : value + offset;
+            Min = isper ? value - (value * offset) : value - offset;
+        }
+
         public T Max { set; get; }
         public T Min { set; get; }
+
+        /// <summary>
+        /// Determines whether a value is within a range.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>true if a value is within a range</returns>
+        public bool Contains(T value)
+        {
+            return value >= Min && value <= Max;
+        }
+
     }
 }
